32b159ea0436899df7cde609f7b7501acf1c2614
Zdenek Vales <tkmushroomer@gmail.com>
2020-07-26 19:31:02 +0200

    Data and R script with some ad-hoc analysis

---
+++ analyze.r
@@ -0,0 +1,111 @@
+library("ggplot2")
+library("scatterplot3d")
+
+# load data
+carData = read.csv(file='data.csv')
+
+# print header and first couple of lines
+names(carData)
+head(carData)
+
+# get data components as num vectors
+years <- as.numeric(carData[["year"]])
+kms <- as.numeric(carData[["kilometers"]])
+prices <- as.numeric(carData[["price"]])
+
+# do stuff
+
+mean(kms)
+median(kms)
+sd(kms)
+quantile(kms)
+
+mean(prices)
+mean(years)
+
+median(prices)
+median(years)
+
+lmres <- lm(price ~ year, data= carData)
+plot(carData$year, carData$price, xlab="Year", ylab="Price [Kè]")
+summary(lmres)
+abline(lmres)
+
+# plot(x=prices, y=kms,
+#      pch=18, 
+#      cex=2, 
+#      col="#69b3a2",
+#      xlab="Price [Kè]", ylab="Kilometers",
+#      main="Price x kilometers"
+# )
+# 
+# plot(x=prices, y=years,
+#      pch=18, 
+#      cex=2, 
+#      col="#69b3a2",
+#      xlab="Price [Kè]", ylab="Year",
+#      main="Price x years"
+# )
+# 
+# plot(x=years, y=kms,
+#      pch=18, 
+#      cex=2, 
+#      col="#69b3a2",
+#      xlab="Year", ylab="Kilometers",
+#      main="Year x kilometers"
+# )
+hist(kms)
+hist(prices)
+hist(years)
+hist(carData$brand)
+
+meanBrandPrices <- tapply(carData$price, carData$brand, mean)
+meanBrandYears <- tapply(carData$year, carData$brand, mean)
+meanBrandKilometers <- tapply(carData$kilometers, carData$brand, mean)
+brandCounts <-(tapply(carData$price, carData$brand, length))
+barplot(sort(meanBrandPrices))
+barplot(meanBrandKilometers)
+brandCountsFiltered = which(brandCounts < 200)
+names(brandCountsFiltered)
+barplot(brandCountsFiltered)
+carDataByBrand <- split(carData, carData$brand)
+names(carDataByBrand)
+skodaMeanPrice <- mean(carDataByBrand$Škoda$price)
+skodaMeanYeras <- mean(carDataByBrand$Škoda$year)
+
+plot(x=carDataByBrand$Škoda$kilometers, y=carDataByBrand$Škoda$price,
+      pch=18, 
+      cex=2, 
+      col="#69b3a2",
+      xlab="Kilometers", ylab="Price [Kè]",
+      main="Price x kilometers (Škoda)"
+ )
+
+plot(x=carDataByBrand$Škoda$year, y=carDataByBrand$Škoda$price,
+     pch=18, 
+     cex=2, 
+     col="#69b3a2",
+     xlab="Kilometers", ylab="Price [Kè]",
+     main="Price x year (Škoda)"
+)
+
+boxplot(price ~ year, 
+        data=carDataByBrand$Škoda, 
+        range=0, 
+        xlab="Manufactured", 
+        ylab="Price [Kè]",
+        main="Price by year (Škoda)")
+
+boxplot(price ~ year, 
+        data=carDataByBrand$Volkswagen, 
+        range=0, 
+        xlab="Manufactured", 
+        ylab="Price [Kè]",
+        main="Price by year (VW)")
+
+boxplot(price ~ year, 
+        data=carDataByBrand$Ford, 
+        range=0, 
+        xlab="Manufactured", 
+        ylab="Price [Kè]",
+        main="Price by year (Ford)")
